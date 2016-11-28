95ce167dcb43e26e79a54bda72f3d2423cbd4c0f
Martti Louhivuori <martti.louhivuori@csc.fi>
2016-11-28 10:50:00 +0200

    Clarify instructions and comments

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 ### Description
 
-Build recipe for the MIC version of GPAW including all dependencies.
+Build recipe for the MIC version of GPAW (incl. all dependencies) that is
+aimed at offloading to Knights Corner (KNC) co-processors:
+https://gitlab.com/gpaw/gpaw/tree/mic
 
 
 ### Install
 
 Go to 'install/' directory and follow the instructions in 'kommand',
 'kommand-pymic', and 'kommand-gpaw' to install the full stack needed by
-GPAW for MICs. Only thing you need to edit in them is the target location
-for the installation (variable $base).
+GPAW for MICs. In principle, the only thing you need to edit is the target
+location for the installation (variable $base) and any module commands (or
+similar) needed to load the compile environment on your system.
+
+In practice, you will also want to take a look at the configuration options,
+patches etc. in the install/setup/ directory and modify as needed to find
+correct libraries on your system and to select appropriate compile flags.
 
 
 ### Test case
--- install/setup/customize-knc.py
+++ install/setup/customize-knc.py
@@ -1,41 +1,36 @@
-#User provided customizations for the gpaw setup
+# Custom GPAW setup for Intel MICs (KNC)
 import os
 
-#Here, one can override the default arguments, or append own
-#arguments to default ones
-#To override use the form
-#     libraries = ['somelib','otherlib']
-#To append use the form
-#     libraries += ['somelib','otherlib']
-
+# use Intel compiler and MPI
 compiler = 'mpiicc'
-mklroot = os.environ['MKLROOT']
+mpicompiler = 'mpiicc'
+mpilinker = 'mpiicc'
 
+# use MKL
+mklroot = os.environ['MKLROOT']
 library_dirs += [mklroot + '/lib/intel64/']
-libraries = ['mkl_intel_lp64', 'mkl_intel_thread', 'mkl_core', 
-             'mkl_lapack95_lp64', 'mkl_scalapack_lp64', 'mkl_blacs_intelmpi_lp64', 
-			 'pthread'
+libraries = ['mkl_intel_lp64', 'mkl_intel_thread', 'mkl_core',
+             'mkl_lapack95_lp64', 'mkl_scalapack_lp64',
+             'mkl_blacs_intelmpi_lp64', 'pthread'
 			]
 
 # libxc
 library_dirs += [os.environ['LIBXCDIR'] + '/lib']
 include_dirs += [os.environ['LIBXCDIR'] + '/include']
 libraries += ['xc']
-			
-# compiler settings for Intel Composer 2013 (ver 14.x)
-extra_compile_args = ['-xHost', '-xAVX', '-O3', '-no-prec-div', '-static', '-std=c99', '-fPIC']
-#extra_compile_args += ['-offload-option,mic,compiler,"-vec-report -openmp"']
-#extra_compile_args += ['-opt-report-phase=offload']
-# extra_compile_args += ['-offload=none']
 
-# for Intel Composer 2015 (ver 15.x)
+# compiler settings for Intel compiler
+extra_compile_args  = ['-xAVX', '-O3']
+extra_compile_args += ['-no-prec-div', '-static', '-std=c99', '-fPIC']
+
+# offload to KNC
 extra_compile_args += ['-qoffload-option,mic,compiler,"-qopenmp"']
 extra_compile_args += ['-qopt-report-phase=offload']
-# extra_compile_args += ['-qoffload=none']
 
 # linker settings for MKL on KNC
 mic_mkl_lib = mklroot + '/lib/mic/'
-extra_link_args += ['-offload-option,mic,link,"-L' + mic_mkl_lib + ' -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lpthread"']
+extra_link_args += ['-offload-option,mic,link,"-L' + mic_mkl_lib \
+        + ' -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lpthread"']
 
 # GPAW defines
 define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
@@ -43,16 +38,6 @@ define_macros += [('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
 define_macros += [("GPAW_ASYNC",1)]
 define_macros += [("GPAW_MPI2",1)]
 
-# use Intel MPI w/ Intel Composer
-mpicompiler = 'mpiicc'
-mpilinker = 'mpiicc'
-
-# platform_id = 'intel64'
-
-#hdf5 = True
-
-# Valid values for scalapack are False, or True:
-# False (the default) - no ScaLapack compiled in
-# True - ScaLapack compiled in
+# use ScaLAPACK
 scalapack = True
 
